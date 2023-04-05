38be6e60f3c555becc1145cf644c12210bbdb6ca
kijen723 <kij2646@gmail.com>
2023-04-05 09:43:31 +0900

    singup function add

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, flash
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "ABCD"
 
 client = MongoClient('localhost', 27017)
 db = client.jranking
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('signup.html')
 
-@app.route('/sign', methods=['POST'])
-def sign_up():
-    # 클라이언트로부터 데이터를 받기
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    name_receive = request.form['name_give']
-    class_receive = request.form['class_give']
-
-    user = {'id': id_receive, 'pw': pw_receive, 'name': name_receive, 'class': class_receive,
-               'time': 0}
-
-    # mongoDB에 데이터를 넣기
-    db.users.insert_one(user)
-
-    return jsonify({'result': 'success'})
+# 회원가입
+@app.route('/signup', methods=['POST'])
+def postUser():
+    if request.method == 'POST':
+        id = request.form['id']
+        password = request.form['password']
+        name = request.form['name']
+        classroom = request.form['classroom']
+        
+        # 이름 입력 확인
+        if name == '':
+            flash("이름을 입력해주세요!")
+            return render_template('signup.html')
+        
+        # id 중복 확인
+        temp = list(db.users.find({}))
+        for user in temp:
+            if id in user['id']:
+                flash("중복된 ID입니다!")
+                return render_template('signup.html')
+        
+        # pw 길이 확인
+        if len(password) < 8:
+            flash("8글자 이상의 비밀번호를 입력해주세요!")
+            return render_template('signup.html')
+        
+        # db에 저장
+        db.users.insert_one({'id':id, 'pw':password, 'name':name, 'classroom':classroom, 'total':0, 'token':''})
+        flash("가입이 완료되었습니다!")
+        return render_template('signup.html')
+    else:
+        return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
