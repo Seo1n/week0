71c9a7521e40addd0ba50d414b38449ea3c91282
Minji Lee <minji0479@gmail.com>
2023-04-06 11:32:24 +0900

    slack mention api working

--- slack.py
+++ slack.py
@@ -2,9 +2,22 @@ from slack_sdk.errors import SlackApiError
 from pymongo import MongoClient
 import requests
 from datetime import date
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Blueprint, Flask
+import ssl
+
+# slack_bp = Blueprint('slack', __name__)
+
 client = MongoClient('mongodb://test:test@localhost',27017)
 
-db = client.jranking
+ssl._create_default_https_context = ssl._create_unverified_context
+
+app = App(token="xoxb-5077704286977-5062165098101-gSOe7FIKFUnUcFtDwvl6rf4X")
+handler = SocketModeHandler(app_token="xapp-1-A051ZGZQDJ8-5058886934326-4ede9ed7d821c7dd9235be4ab25cbdebfc20bdd2ddb0bec672c9da42068a73f7", app=app)
+
+db = client.jungle
 cursor_all = db.users.find({})
 all_data = []
 for document in cursor_all:
@@ -13,14 +26,15 @@ all_data.sort(key=lambda x: x.get('total'),reverse=True)
 number_one = all_data[0]['name']
 starttime = date.today().strftime("%Y-%m-%d")
 time = str(starttime)
-token = "xoxb-5077704286977-5062165098101-NAyggJaLDzTNZo8SBJiZ5jNK"
-channel = "#ranking-bot"
-text = time + " 의 공부시간 1위는 " + number_one
+result = time + " 의 공부시간 1위는 " + number_one
 
-try :
-    response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={"Authorization": "Bearer "+token},
-        data={"channel": channel,"text": text})
-    print(response.text)
-except SlackApiError as e:
-    print("Error sending message: {}".format(e))
+@app.event("app_mention")
+def handle_mention(event, say):
+    try:
+        # Send a reply to the user who mentioned the bot
+        say(f"<@{event['user']}> {result}")
+    except Exception as e:
+        print(f"Error sending message: {e}")
+
+if __name__ == "__main__":
+    handler.start()
